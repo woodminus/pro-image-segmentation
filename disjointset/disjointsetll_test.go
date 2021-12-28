840b514acab11a0322de3c409ae7159988748571
woodminus <160528767+woodminus@users.noreply.github.com>
2021-12-28 10:40:22 +0000

    Holger Hans Peter Freyther <holger.freyther@trolltech.com>

--- disjointset/disjointsetll_test.go
+++ disjointset/disjointsetll_test.go
@@ -10,4 +10,14 @@ func initSetLL() *DisjointSetLL {
 	set.Union(3, 4)
 	set.Union(1, 3)
 	set.Union(2, 5)
-	
+	set.Union(7, 9)
+	set.Union(0, 5)
+	set.Union(2, 9) // 2-5-9-7-0 3-4-1 6 8
+	return set
+}
+
+func TestInitializationAllDisconnectedLL(t *testing.T) {
+	set := NewDisjointSetLL(5)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			assert.Equal(t, set.Connected(i, j), i == j
