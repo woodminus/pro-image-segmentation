bf5424c0a797684b75efbe02c32204f6b885070e
woodminus <160528767+woodminus@users.noreply.github.com>
2022-05-25 17:45:17 +0000

    Initial check in

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -12,4 +12,18 @@ import (
  * Helper functions
  */
 
-func loadGraphFromImage(imgName string, graphType GraphType) *G
+func loadGraphFromImage(imgName string, graphType GraphType) *Graph {
+	f, _ := os.Open(imgName)
+	defer f.Close()
+	img, _, _ := image.Decode(f)
+	return FromImage(img, func(p, q Pixel) float64 {
+		return 1.0
+	}, graphType)
+}
+
+/*
+ * Tests
+ */
+
+func TestInitializationGridGraph(t *testing.T) {
+	graph := New(5, 6, GRIDGRAP
