361b898e3eb60b9152d9f20d3e1ccfef53b2d9cf
woodminus <160528767+woodminus@users.noreply.github.com>
2022-08-20 13:53:52 +0000

    why

--- segmentation/segmentation.go
+++ segmentation/segmentation.go
@@ -1,3 +1,18 @@
 /**
  * This is probably the most important package in the repository. It contains
- * all the segmentation algorithms and the utilities to transfo
+ * all the segmentation algorithms and the utilities to transform from an
+ * image to a graph and from a DisjointSet forest to an image.
+ */
+package segmentation
+
+import (
+	"fmt"
+	"github.com/miguelfrde/image-segmentation/disjointset"
+	"github.com/miguelfrde/image-segmentation/graph"
+	"github.com/miguelfrde/imaging"
+	"image"
+	"time"
+)
+
+/**
+ *
